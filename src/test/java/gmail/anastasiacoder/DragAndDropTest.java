1221a0aa2b47715d721eb7f3190d84554f3c3714
AnastasiaCoder <mstitelnica1999@gmail.com>
2021-11-04 18:37:13 +0700

    added dragAndDrop test

---
+++ src/test/java/gmail/anastasiacoder/DragAndDropTest.java
@@ -0,0 +1,24 @@
+package gmail.anastasiacoder;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void dragAndDropCheck(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+    }
+}
